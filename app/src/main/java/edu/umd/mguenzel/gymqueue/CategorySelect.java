a7b455df7801c0652236ef4ac6e22f7ee31c50b0
Sandra <sandrasoltz@gmail.com>
2016-04-21 17:35:43 -0400

    category select page
    new page!

---
+++ app/src/main/java/edu/umd/mguenzel/gymqueue/CategorySelect.java
@@ -0,0 +1,22 @@
+package edu.umd.mguenzel.gymqueue;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by sandrasoltz on 4/21/16.
+ */
+public class CategorySelect extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Button clear= (Button) findViewById(R.id.cardio_button);
+
+
+    }
+
+
+}
