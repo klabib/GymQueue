b6110378923dc190be20b2097510b3c498094fed
Sandra <sandrasoltz@gmail.com>
2016-04-27 17:37:21 -0400

    whatever

--- app/src/main/java/edu/umd/mguenzel/gymqueue/SignUpActivity.java
+++ app/src/main/java/edu/umd/mguenzel/gymqueue/SignUpActivity.java
@@ -24,8 +24,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
