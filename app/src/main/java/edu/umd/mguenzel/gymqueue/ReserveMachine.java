d175c259fbfd3f3cee929b5ad9e60d5b41e9e99d
mrguenzel <mrguenzel@gmail.com>
2016-05-03 23:41:33 -0400

    reserve machine updates

--- app/src/main/java/edu/umd/mguenzel/gymqueue/ReserveMachine.java
+++ app/src/main/java/edu/umd/mguenzel/gymqueue/ReserveMachine.java
@@ -3,28 +3,40 @@ package edu.umd.mguenzel.gymqueue;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class ReserveMachine extends Activity {
 
     private final String[] RESERVABLE_TIMES = new String[57];
-    String uid;
-    String machine;
-    Firebase mFirebase;
-    String month;
-    int day;
+    private String uid, machine;
+    private Firebase mFirebase;
+    private int day, month, year;
     private boolean isChecking = true;
     private int mCheckedId = R.id.one;
     private RadioGroup mFirstGroup;
     private RadioGroup mSecondGroup;
+    private Button submit;
+
+    private RadioGroup rg_hour1, rg_hour2, rg_min, rg_ampm;
+    private RadioButton rb_hour, rb_min, rb_ampm;
+
+    private Calendar cal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,19 @@ public class ReserveMachine extends Activity {
         Intent intent = getIntent();
         uid = intent.getStringExtra("UID");
         machine = intent.getStringExtra("machine");
+
+        submit = (Button) findViewById(R.id.submit);
+        rg_hour1 = (RadioGroup) findViewById(R.id.hour1);
+        rg_hour2 = (RadioGroup) findViewById(R.id.hour2);
+        rg_min = (RadioGroup) findViewById(R.id.minute);
+        rg_ampm = (RadioGroup) findViewById(R.id.ampm);
+
+        Date d = new Date();
+        cal = Calendar.getInstance();
+        cal.setTime(d);
+        month = cal.get(Calendar.MONTH) + 1;
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        year = cal.get(Calendar.YEAR);
 
         //setting up the reservable times, from 8:00-22:00
         int index = 0;
@@ -65,13 +90,72 @@ public class ReserveMachine extends Activity {
             }
         });
 
-        Date d = new Date();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(d);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rg_hour1.getCheckedRadioButtonId() == -1 && rg_hour2.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(getApplicationContext(), "Please select an hour", Toast.LENGTH_LONG).show();
+                } else if (rg_min.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(getApplicationContext(), "Please select a minute interval", Toast.LENGTH_LONG).show();
+                } else if (rg_ampm.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(getApplicationContext(), "Please select either am or pm", Toast.LENGTH_LONG).show();
+                } else { //valid
+                    String min = (String) ((RadioButton) findViewById(rg_min.getCheckedRadioButtonId())).getText();
+                    String ampm = (String) ((RadioButton) findViewById(rg_ampm.getCheckedRadioButtonId())).getText();
+                    String hour = "";
+
+                    if (rg_hour1.getCheckedRadioButtonId() == -1) {
+                        hour = (String) ((RadioButton) findViewById(rg_hour2.getCheckedRadioButtonId())).getText();
+                    } else {
+                        hour = (String) ((RadioButton) findViewById(rg_hour1.getCheckedRadioButtonId())).getText();
+                    }
+                    //hour = toMilitaryTime(hour, ampm);
+
+                    //if time is later today
+                        //reserve for today
+                    //else
+                        //reserve for tomorrow
+
+                    String str = month + "/" + day + "/" + year + " " + hour + min  + " " + ampm;
+                    DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+                    Date date, currDate = new Date();
+                    try {
+                        date = formatter.parse(str);
+                        Log.i("date", date.toString());
+                    } catch (ParseException e) {
+                        Log.i("exception", "Time parse error (should never get here)");
+                        return;
+                    }
+
+                    String monthName = getMonthName(month);
 
 
+
+                    if (date.after(currDate)) { //time is later today, reserve for today
+                        Log.i("test", "today");
+                        //create reservation in firebase
+
+                        mFirebase.child("Reservations").child(monthName).child(Integer.toString(day)).child(hour + min + " " + ampm).child(machine).setValue(1);
+
+                        //IN ON DATA CHANGED
+                        //if reservation value at time > max
+                            //change it back and display toast
+
+                    } else { //time passed today, reserve for tomorrow
+                        Log.i("test", "tomorrow" +  " " + currDate.toString());
+
+                        //TODO: increment day properly
+                        mFirebase.child("Reservations").child(monthName).child(Integer.toString(day + 1)).child(hour + min + " " + ampm).child(machine).setValue(1);
+                    }
+
+
+                }
+            }
+        });
+
+
+        //listeners to help with selecting hours
         mFirstGroup = (RadioGroup) findViewById(R.id.hour1);
         mSecondGroup = (RadioGroup) findViewById(R.id.hour2);
 
@@ -98,6 +182,52 @@ public class ReserveMachine extends Activity {
                 isChecking = true;
             }
         });
+    }
+
+    private String getMonthName(int month) {
+        if (month == 1) {
+            return "January";
+        } else if (month == 2) {
+            return "February";
+        } else if (month == 3) {
+            return "March";
+        } else if (month == 4) {
+            return "April";
+        } else if (month == 5) {
+            return "May";
+        } else if (month == 6) {
+            return "June";
+        } else if (month == 7) {
+            return "July";
+        } else if (month == 8) {
+            return "August";
+        } else if (month == 9) {
+            return "September";
+        } else if (month == 10) {
+            return "October";
+        } else if (month == 11) {
+            return "November";
+        } else if (month == 12) {
+            return "December";
+        }
+
+        return "never here";
+    }
+
+    private String toMilitaryTime(String time, String ampm) {
+        int t = Integer.parseInt(time);
+        if (ampm.equals("am")) {
+            if (t == 12) {
+                return  "00";
+            }
+            return time;
+        } else { //pm, convert to military
+            if (t == 12) {
+                return time;
+            }
+            t += 12;
+            return Integer.toString(t);
+        }
     }
 }
 
