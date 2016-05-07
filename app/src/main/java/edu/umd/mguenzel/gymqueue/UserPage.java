761c1d4fe167948761643e7fb0269a641f3ddaf5
Alan Zhang <A.zhang123@gmail.com>
2016-05-07 13:38:50 -0400

    userpage edits

--- app/src/main/java/edu/umd/mguenzel/gymqueue/UserPage.java
+++ app/src/main/java/edu/umd/mguenzel/gymqueue/UserPage.java
@@ -22,6 +22,8 @@ public class UserPage extends Activity{
     private TextView elliptical_num, rec_bike_num, stair_num, stat_bike_num, treadmill_num, deadlift_num,
             lat_pull_num, row_mach_num, bench_press_norm_num, chest_fly_num, bench_press_incl_num,
             bench_press_decl_num, calf_raise_num, leg_ext_num, leg_press_num, squat_num, should_press_num;
+    private int counter = 0;
+    private long[] numbers;
     private Firebase mFirebase;
     private String uid;
 
@@ -33,6 +35,7 @@ public class UserPage extends Activity{
         mFirebase = new Firebase("https://gymqueue.firebaseio.com");
 
         elliptical_num = (TextView)findViewById(R.id.elliptical_num);
+        numbers = new long[16];
 
         uid = getIntent().getStringExtra("UID");
 
@@ -48,32 +51,39 @@ public class UserPage extends Activity{
         mFirebase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot child : dataSnapshot.child("Users").child(uid).getChildren()) {
+                    numbers[counter] = (long) child.getValue();
+                    counter++;
+                }
+                /*
                 //cardio
-                elliptical = (int) dataSnapshot.child("User").child(uid).child("Elliptical").getValue();
-                rec_bike = (int) dataSnapshot.child("User").child(uid).child("Recumbent Bike").getValue();
-                stair = (int) dataSnapshot.child("User").child(uid).child("Stair Stepper").getValue();
-                stat_bike = (int) dataSnapshot.child("User").child(uid).child("Stationary Bike").getValue();
-                treadmill = (int) dataSnapshot.child("User").child(uid).child("Treadmill").getValue();
+                elliptical = (int) dataSnapshot.child("Users").child(uid).child("Elliptical").getValue();
+                rec_bike = (int) dataSnapshot.child("Users").child(uid).child("Recumbent Bike").getValue();
+                stair = (int) dataSnapshot.child("Users").child(uid).child("Stair Stepper").getValue();
+                stat_bike = (int) dataSnapshot.child("Users").child(uid).child("Stationary Bike").getValue();
+                treadmill = (int) dataSnapshot.child("Users").child(uid).child("Treadmill").getValue();
 
                 //back
-                deadlift = (int) dataSnapshot.child("User").child(uid).child("Deadlift").getValue();
-                lat_pull = (int) dataSnapshot.child("User").child(uid).child("Lat Pulldown").getValue();
-                row_mach = (int) dataSnapshot.child("User").child(uid).child("Row Machine").getValue();
+                deadlift = (int) dataSnapshot.child("Users").child(uid).child("Deadlift").getValue();
+                lat_pull = (int) dataSnapshot.child("Users").child(uid).child("Lat Pulldown").getValue();
+                row_mach = (int) dataSnapshot.child("Users").child(uid).child("Row Machine").getValue();
 
                 //chest
-                bench_press_norm = (int) dataSnapshot.child("User").child(uid).child("Bench Press").getValue();
-                chest_fly = (int) dataSnapshot.child("User").child(uid).child("Chest Flys").getValue();
-                bench_press_incl = (int) dataSnapshot.child("User").child(uid).child("Incline Bench Press").getValue();
-                bench_press_decl = (int) dataSnapshot.child("User").child(uid).child("Decline Bench Press").getValue();
+                bench_press_norm = (int) dataSnapshot.child("Users").child(uid).child("Bench Press").getValue();
+                chest_fly = (int) dataSnapshot.child("Users").child(uid).child("Chest Flys").getValue();
+                bench_press_incl = (int) dataSnapshot.child("Users").child(uid).child("Incline Bench Press").getValue();
+                bench_press_decl = (int) dataSnapshot.child("Users").child(uid).child("Decline Bench Press").getValue();
 
                 //legs
-                calf_raise = (int) dataSnapshot.child("User").child(uid).child("Calf Raises").getValue();
-                leg_ext = (int) dataSnapshot.child("User").child(uid).child("Leg Extension").getValue();
-                leg_press = (int) dataSnapshot.child("User").child(uid).child("Leg Press").getValue();
-                squat = (int) dataSnapshot.child("User").child(uid).child("Squat").getValue();
+                calf_raise = (int) dataSnapshot.child("Users").child(uid).child("Calf Raises").getValue();
+                leg_ext = (int) dataSnapshot.child("Users").child(uid).child("Leg Extension").getValue();
+                leg_press = (int) dataSnapshot.child("Users").child(uid).child("Leg Press").getValue();
+                squat = (int) dataSnapshot.child("Users").child(uid).child("Squat").getValue();
 
                 //shoulders
-                should_press = (int) dataSnapshot.child("User").child(uid).child("Shoulder Press").getValue();
+                should_press = (int) dataSnapshot.child("Users").child(uid).child("Shoulder Press").getValue();
+                */
             }
 
             @Override
@@ -82,9 +92,7 @@ public class UserPage extends Activity{
             }
         });
 
-        Toast.makeText(UserPage.this, "elliptical: " + elliptical, Toast.LENGTH_LONG).show();
-
-        elliptical_num.setText(elliptical);
+        elliptical_num.setText("" + numbers[0]);
     }
 
 }
